f5bbbc882fb8c0154e677efe6c8b1d5c075a9b2f
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-12-03 16:52:04 -0700

    merge with job-posts

---
+++ src/components/JobFeed/JobFeed.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class ViewJobs extends Component {
+constructor(){
+    super();
+    this.state = {
+        jobs: [{title: 'test title1',
+            description: 'test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description',
+            start_date: 'test start date',
+            estimation: 'test estimation',
+            pay: 'test pay'},
+            {title: 'test title2',
+            description: 'test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description',
+            start_date: 'test start date',
+            estimation: 'test estimation',
+            pay: 'test pay'},
+            {title: 'test title3',
+            description: 'test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description',
+            start_date: 'test start date',
+            estimation: 'test estimation',
+            pay: 'test pay'}]
+    }
+}
+
+componentDidMount(){
+    axios.get('/').then(res => {
+        this.setState({jobs: res.data})
+    })
+}
+
+render() {
+    let displayJobs = this.state.jobs.map(job => {
+        let splitDescription = job.description.split(' ')
+        let trimDescription = splitDescription.slice(10, (splitDescription.length-40)).join(' ')
+        return <div>
+            <h1>{job.title}</h1>
+            <p>{trimDescription}...</p>
+            <p>${job.pay}</p>
+        </div>
+    })
+    return (
+        <div>
+            <br /> <br /> <br />
+            {displayJobs}
+        </div>
+    )}
+}
+
+export default ViewJobs;
