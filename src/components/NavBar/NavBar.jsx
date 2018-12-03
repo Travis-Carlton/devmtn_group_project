685d6e0cf942f4de91768cf5d8a0b11665515e25
TCoXXV <tccodingxxv@gmail.com>
2018-12-02 19:52:06 -0700

    added front page picture and tablet and iphone678plues responsive nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,18 +3,45 @@ import './NavBar.scss';
 import {Link} from 'react-router-dom';
 
 export default class NavBar extends Component {
+    constructor(){
+        super();
+        this.state = {
+            showTabs: false,
+        }
+    }
+
+    toggleTabs = ()=>{
+        this.setState({
+            showTabs: !this.state.showTabs
+        })
+    }
+
     render() {
         return (
             <div className='navbarp'>
                 <div className='navbarc'>
                     <div className='navbarlogo'>
-                        Logo
+                        DevWay
                     </div>
                     <div className="navbarcc">
                         <Link to='/'>Home</Link>
                         {/* a tags are place holders for visual */}
+                        <Link to='/#'>How It Works</Link>
                         <Link to='/login'>Log In/Sign Up</Link>
+                        <Link to='/#'>Link</Link>
+                        <Link to='/#'>Link</Link>
                     </div>
+                        <button onClick={this.toggleTabs} className='mobiletab'>❖</button>
+                        {this.state.showTabs &&
+                        <div className="showtabs">
+                            <div>
+                                <Link to='/'>Home</Link>
+                                <Link to='/#'>How It Works</Link>
+                                <Link to='/login'>Log In/Sign Up</Link>
+                                <Link to='/#'>Link</Link>
+                                <Link to='/#'>Link</Link>
+                            </div>
+                        </div>}
                 </div>
             </div>
         );
