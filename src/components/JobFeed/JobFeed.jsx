ecfd9d6cba87cc0ff4813a770c07bc65297b3018
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-12-04 11:47:07 -0700

    duplicate axios call fix

--- src/components/JobFeed/JobFeed.jsx
+++ src/components/JobFeed/JobFeed.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './JobFeed.scss';
-import axios from 'axios';
 import {Link} from 'react-router-dom';
 
 class JobFeed extends Component {
