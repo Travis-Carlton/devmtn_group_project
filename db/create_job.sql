b952f53793740a0c91bc5d3f011aaa2c73dbf21c
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-12-04 11:49:15 -0700

    accepted controller change

--- db/create_job.sql
+++ db/create_job.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 insert into jobs (client_id, title, description, start_date, estimation, pay, job_email)
 values($1, $2, $3, $4, $5, $6, $7);
-=======
-insert into jobs ( client_id, title, description, start_date, estimation, pay)
-values($1, $2, $3, $4, $5, $6);
->>>>>>> job-page
