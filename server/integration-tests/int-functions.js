e4ee65b88ed1d62fe2b1453b10ab1ed7a7f06a4a
Hunter Carrico <huntercarrico@Hunters-MacBook-Air.local>
2018-12-14 17:20:22 -0700

    finished testing

--- server/integration-tests/int-functions.js
+++ server/integration-tests/int-functions.js
@@ -5,5 +5,30 @@ module.exports = {
         return axios.get('https://swapi.co/api/people').then(person => {
             return person.data
         })
+    },
+    getLukeSkywalker(){
+        return axios.get('https://swapi.co/api/people/1').then(person => {
+            return person.data
+        })
+    },
+    getAllCards(){
+        return axios.get('https://api.pokemontcg.io/v1/cards').then(pokemon => {
+            return pokemon.data
+        })
+    },
+    getCharizard(){
+        return axios.get('https://api.pokemontcg.io/v1/cards?name=charizard').then(charizard => {
+            return charizard.data.cards[0]
+        })
+    },
+    getMagicCard1(){
+        return axios.get('https://api.magicthegathering.io/v1/cards').then(magic => {
+            return magic.data.cards[0]
+        })
+    },
+    getAllMagicCards(){
+        return axios.get('https://api.magicthegathering.io/v1/cards').then(magicAll => {
+            return magicAll.data.cards
+        })
     }
 }
--- server/integration-tests/int-functions.test.js
+++ server/integration-tests/int-functions.test.js
@@ -7,5 +7,40 @@ describe('intFunctions', () => {
             const persons = await intFunctions.getAllCharacters()
             expect(persons.results.length).toBe(10)
         })
+    }),
+    describe('getLukeSkywalker', () => {
+        it('returns Luke', async() => {
+            const persons = await intFunctions.getLukeSkywalker()
+            // console.log(persons)
+            expect(persons.name).toBe('Luke Skywalker')
+        })
+    }),
+    describe('getAllPokemon', () => {
+        it('returns Pokemon', async() => {
+            const pokemon = await intFunctions.getAllCards()
+            // console.log(pokemon.cards.length)
+            expect(pokemon.cards.length).toBe(100)
+        })
+    }),
+    describe('getCharizard', () => {
+        it('returns Charizard Name', async() => {
+            const charizard = await intFunctions.getCharizard()
+            expect(charizard.name).toBe('Charizard-EX')
+        })
+    }),
+    describe('getMagicCards', () => {
+        it('returns magic card 1 name', async() => {
+            const magic = await
+            intFunctions.getMagicCard1()
+            expect(magic.subtypes.length).toBe(1)
+        })
+    }),
+    describe('getAllDaMAgic', () => {
+        it('returns all the magic cards', async() => {
+            const magicCards = await
+            intFunctions.getAllMagicCards()
+            console.log(magicCards.length)
+            expect(magicCards.length).toBe(100)
+        })
     })
 })
